f88b503311e5d04ea57db9a76c20a2e1d40c74be
Pegasa-WQ <sviderek442@gmail.com>
2021-08-10 09:27:28 +0700

    First commit.

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,16 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'login' },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    meta: { layout: 'main' },
+    component: () => import('../views/Projects.vue')
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from './auth'
+import bugs from './bugs'
+import testCases from './testCases'
+import projects from './projects'
+import testReview from './testReview'
+import roles from './roles'
+import testRun from './testRun'
+import statisticks from './statistics'
+import testSuite from './testSuite'
+import testPlan from './testPlan.js'
+import users from './users'
 
 Vue.use(Vuex)
 
@@ -11,5 +22,16 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    auth,
+    bugs,
+    testCases,
+    projects,
+    testReview,
+    roles,
+    testRun,
+    statisticks,
+    testSuite,
+    testPlan,
+    users
   }
 })
